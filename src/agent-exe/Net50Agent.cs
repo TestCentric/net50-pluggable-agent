ee404e52ef88d747ce94bc74d5992c9d3abc96b0
Charlie Poole <charliepoole@gmail.com>
2023-09-09 07:21:19 -0700

    Reference beta2 engine; allow running agent from command-line

---
+++ src/agent-exe/Net50Agent.cs
@@ -0,0 +1,12 @@
+// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE file in root directory.
+// ***********************************************************************
+
+namespace TestCentric.Agents
+{
+    public class Net50Agent : TestCentricAgent<Net50Agent>
+    {
+        public static void Main(string[] args) => TestCentricAgent<Net50Agent>.Execute(args);
+    }
+}
